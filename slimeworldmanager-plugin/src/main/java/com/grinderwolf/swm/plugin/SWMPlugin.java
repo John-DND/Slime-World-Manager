20234e613e5c7fde10a043893cc4810fd1232c53
thamid-gamer <60953955+thamid-gamer@users.noreply.github.com>
2020-10-11 19:43:53 -0400

    Working plugin

--- slimeworldmanager-plugin/src/main/java/com/grinderwolf/swm/plugin/SWMPlugin.java
+++ slimeworldmanager-plugin/src/main/java/com/grinderwolf/swm/plugin/SWMPlugin.java
@@ -10,16 +10,6 @@ import com.grinderwolf.swm.api.world.properties.SlimeProperties;
 import com.grinderwolf.swm.api.world.properties.SlimePropertyMap;
 import com.grinderwolf.swm.nms.CraftSlimeWorld;
 import com.grinderwolf.swm.nms.SlimeNMS;
-import com.grinderwolf.swm.nms.v1_10_R1.v1_10_R1SlimeNMS;
-import com.grinderwolf.swm.nms.v1_11_R1.v1_11_R1SlimeNMS;
-import com.grinderwolf.swm.nms.v1_12_R1.v1_12_R1SlimeNMS;
-import com.grinderwolf.swm.nms.v1_13_R1.v1_13_R1SlimeNMS;
-import com.grinderwolf.swm.nms.v1_13_R2.v1_13_R2SlimeNMS;
-import com.grinderwolf.swm.nms.v1_14_R1.v1_14_R1SlimeNMS;
-import com.grinderwolf.swm.nms.v1_15_R1.v1_15_R1SlimeNMS;
-import com.grinderwolf.swm.nms.v1_8_R3.v1_8_R3SlimeNMS;
-import com.grinderwolf.swm.nms.v1_9_R1.v1_9_R1SlimeNMS;
-import com.grinderwolf.swm.nms.v1_9_R2.v1_9_R2SlimeNMS;
 import com.grinderwolf.swm.plugin.commands.CommandManager;
 import com.grinderwolf.swm.plugin.config.ConfigManager;
 import com.grinderwolf.swm.plugin.config.WorldData;
@@ -163,26 +153,6 @@ public class SWMPlugin extends JavaPlugin implements SlimePlugin {
         String nmsVersion = version.substring(version.lastIndexOf('.') + 1);
 
         switch (nmsVersion) {
-            case "v1_8_R3":
-                return new v1_8_R3SlimeNMS();
-            case "v1_9_R1":
-                return new v1_9_R1SlimeNMS();
-            case "v1_9_R2":
-                return new v1_9_R2SlimeNMS();
-            case "v1_10_R1":
-                return new v1_10_R1SlimeNMS();
-            case "v1_11_R1":
-                return new v1_11_R1SlimeNMS();
-            case "v1_12_R1":
-                return new v1_12_R1SlimeNMS();
-            case "v1_13_R1":
-                return new v1_13_R1SlimeNMS();
-            case "v1_13_R2":
-                return new v1_13_R2SlimeNMS();
-            case "v1_14_R1":
-                return new v1_14_R1SlimeNMS();
-            case "v1_15_R1":
-                return new v1_15_R1SlimeNMS();
             case "v1_16_R2":
                 return new v1_16_R2SlimeNMS();
             default:
